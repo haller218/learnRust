c4bcc849319d4237b8d3615ca7636b11dd7734a1
haller218 <eliseu42lucena@gmail.com>
2019-08-13 11:04:29 -0300

    real guess program works!

--- src/2_guessing_game/src/main.rs
+++ src/2_guessing_game/src/main.rs
@@ -1,4 +1,3 @@
-
 use std::io;
 use std::cmp::Ordering;
 use rand::Rng;
@@ -7,27 +6,31 @@ use rand::Rng;
 
 fn main(){
 
-
-    let mut guess = String::new();
+    println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1,101);
 
-    println!("Please enter to a number: ");
+//    println!("The Secret number is: {} ", secret_number);
+    loop {
+        println!("Please input your guess.");
 
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line");
+        let mut guess = String::new();
 
-    // ---- snip ----
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
 
-    println!("You guessed: {}", guess);
+        let guess: u32 = guess.trim().parse()
+            .expect("Please type a number!");
 
-    match guess.cmp(&secret_number) {
+        println!("You guessed: {}",guess);
 
-        Ordering::Less => println!("Too small"),
-        Ordering::Greater => println!("Too big"),
-        Ordering::Equal => println!("You Wins!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println! ("Too small"),
+            Ordering::Greater => println! ("Too big"),
+            Ordering::Equal => {
+                println! ("You Wins!");
+                break;
+            }
+        }
     }
-    
 }
-
-
