39f43e578006e33f08be2b5e0cb8896aa8e4d4a3
haller218 <eliseu42lucena@gmail.com>
2019-08-13 15:26:21 -0300

    verifying the mutability of variables and imutability

--- src/LESON3/variables/src/main.rs
+++ src/LESON3/variables/src/main.rs
@@ -1,11 +1,50 @@
-fn main() {
+fn tests_using_let () {
 
-    let x = 5;
+    let mut x = 5;
 
     println!("The value of x is: {}",x);
 
     x = 6;
 
     println!("The value of x is: {}",x);
+
+}
+
+
+fn test_using_const () {
+
+    const MAX_POINTS: u32 = 100_000;
+
+    println!("Max const {}", MAX_POINTS);
+
+}
+
+fn test_shadowed () {
+
+    let x = 5;
+
+    let x = x + 1;
+
+    let x = x * 2;
+
+    println!("The value of x is: {}", x);
+}
+
+fn test_change_datatype () {
+
+    let spaces = "  ";
+
+    let spaces = spaces.len();
+}
+
+fn main () {
+
+    tests_using_let();
+
+    test_using_const();
+
+    test_shadowed ();
+
+    test_change_datatype ();
 
 }
