3ad9bba74d6a850d25bea9bbe89ddc9e6dddaa54
haller218 <eliseu42lucena@gmail.com>
2019-08-13 16:04:16 -0300

    float test, numeric operations, and booleans

--- src/LESON3/dataTypes/src/main.rs
+++ src/LESON3/dataTypes/src/main.rs
@@ -13,8 +13,80 @@ fn test_parsing_type() {
     
 }
 
+// scalar types represents a single value. Rust has four primary scalar types:
+// integers, floating-point numbers, Booleans, and characters. You may
+// recognize these from other programing languages. Let's jump into
+// how they work in Rust
+
+// Integer Types
+
+
+// 8-bit 	i8 	u8
+// 16-bit 	i16 	u16
+// 32-bit 	i32 	u32
+// 64-bit 	i64 	u64
+// 128-bit 	i128 	u128
+// arch 	isize 	usize
+
+
+// Number literals 	Example
+// Decimal 	98_222
+// Hex 	0xff
+// Octal 	0o77
+// Binary 	0b1111_0000
+// Byte (u8 only) 	b'A'
+
+
+fn float_point_test () {
+
+    let x = 2.0; // f64
+
+    let y: f32 = 3.0; // f32
+
+    println!("value of float64 x: {}, value of float32 {}", x, y);
+}
+
+fn numeric_operations () {
+
+    // adition
+    let sum = 5 + 10;
+
+    // subtraction
+    let diference = 95.5 - 4.3;
+
+    // multiplication
+    let product = 4 * 30;
+
+    // division
+    let quotient = 57.7 / 32.2;
+
+    // remainder
+    let remainder = 43 % 5;
+
+    println!("Results: {}, {}, {}, {}, {}", sum, diference, product,
+             quotient, remainder)
+    
+}
+
+//// the boolean type
+
+fn booleans () {
+
+
+    let t = true;
+
+    let f: bool = false; // the explicity type anotation
+
+    println!("Values: {}, {}", t, f);
+}
+
 fn main () {
 
     test_parsing_type();
 
+    float_point_test();
+
+    numeric_operations();
+
+    booleans();
 }
