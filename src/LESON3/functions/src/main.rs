61d483f39756d0cc6639e4d471ee7360b6f634c5
haller218 <eliseu42lucena@gmail.com>
2019-08-13 17:52:10 -0300

    function and add function call

---
+++ src/LESON3/functions/src/main.rs
@@ -0,0 +1,15 @@
+fn another_function() {
+
+    println!("yeah, another function call.!");
+}
+
+////
+// function parameters
+
+
+fn main() {
+    
+    println!("Hello, world!");
+    
+    another_function();
+}
