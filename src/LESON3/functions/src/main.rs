8acffc9fdb4aec6d51b097582011e0afc0a8867e
haller218 <eliseu42lucena@gmail.com>
2019-08-13 18:18:54 -0300

    function statement bodies statements and so

--- src/LESON3/functions/src/main.rs
+++ src/LESON3/functions/src/main.rs
@@ -24,7 +24,30 @@ fn _test_functions() {
 ////
 // statement do not return a value.
 
-fn main () {
+fn _statement_function () {
 
     let _a: i32 = 42;
+}
+
+////
+// Function Bodies Contain Statements and Expressions
+
+fn _functions_bodies_statements () {
+
+    let x = 5;
+
+    let y = {
+
+        let x = 3;
+
+        x + 1
+    };
+
+    println!("the value of y is: {}", y);
+}
+
+
+fn main () {
+
+    // https://doc.rust-lang.org/book/ch03-03-how-functions-work.html#functions-with-return-values
 }
