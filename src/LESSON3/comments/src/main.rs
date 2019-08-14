bcca755184a85757ca87bbff87fd7874e5df3601
haller218 <eliseu42lucena@gmail.com>
2019-08-14 09:56:06 -0300

    add comments to exemple file

--- src/LESSON3/comments/src/main.rs
+++ src/LESSON3/comments/src/main.rs
@@ -1,3 +1,16 @@
+// So we’re doing something complicated here, long enough that we need
+// multiple lines of comments to do it! Whew! Hopefully, this comment will
+// explain what’s going on.
+
+fn _main_data_begin() {
+    let lucky_number = 7; // I’m feeling lucky today
+}
+
+fn _main_data_somethingelse() {
+    // I’m feeling lucky today
+    let lucky_number = 7;
+}
+
 
 
 // hello, world
